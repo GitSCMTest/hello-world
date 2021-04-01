ef7db420ecedeab99c7b43af1db4c9388f25cefb
GitSCMTest <80455047+GitSCMTest@users.noreply.github.com>
2021-04-01 22:49:47 +0530

    updating java file

--- test.java
+++ test.java
@@ -2,11 +2,7 @@ class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
 
-        //us2
-        System.out.println( "Here I am adding some extra changes" );
-		
-		 System.out.println( "Here I am to check the local repo " );
-	     System.out.println( "Here I am to check the local repo changes111111" );
+System.out.println("Need some more code to commit);
     }
     }
 }
